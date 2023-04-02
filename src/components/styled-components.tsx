d1614e473bcfa4df266087df5d6b494eb5d390f4
Qudusayo <qqudusayo@gmail.com>
2023-04-02 23:41:33 +0100

    chore: add sequence page

--- src/components/styled-components.tsx
+++ src/components/styled-components.tsx
@@ -66,7 +66,7 @@ export const LinkButton = styled.span`
   align-items: center;
   justify-content: center;
   color: #e0e7ff;
-  background-color: #EF3E36;
+  background-color: #ef3e36;
   width: 100%;
   text-align: center;
   border-radius: 0.5em;
@@ -92,4 +92,42 @@ export const Button = styled.button<{ bg?: string; color?: string }>`
   margin-bottom: 1em;
   align-items: center;
   justify-content: center;
+`;
+
+export const RefreshBlock = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 1em 0;
+  gap: 0.5em;
+  font-size: 0.95em;
+  cursor: pointer;
+`;
+
+export const Select = styled.select`
+  display: block;
+  width: 100%;
+  outline: none;
+  background: transparent;
+  border-radius: 5px;
+  height: 45px;
+  padding: 0 10px;
+  margin-top: 10px;
+  font-size: 1em;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  background-image: url("data:image/svg+xml;utf8,<svg width='9' height='6' viewBox='0 0 9 6' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M5.07671 5.33928L8.86404 1.55188C8.9517 1.46428 9 1.34735 9 1.22266C9 1.09798 8.9517 0.981043 8.86404 0.893445L8.58519 0.614529C8.40349 0.433037 8.10818 0.433037 7.92675 0.614529L4.74638 3.7949L1.56248 0.611C1.47482 0.523402 1.35795 0.475036 1.23333 0.475036C1.10858 0.475036 0.991712 0.523402 0.903976 0.611L0.625198 0.889915C0.537531 0.977582 0.489235 1.09445 0.489235 1.21913C0.489235 1.34382 0.537531 1.46075 0.625198 1.54835L4.41599 5.33928C4.50393 5.42709 4.62135 5.47531 4.74617 5.47504C4.87148 5.47531 4.98883 5.42709 5.07671 5.33928Z' fill='white'/></svg>");
+  background-repeat: no-repeat;
+  background-position-x: 95%;
+  background-position-y: 50%;
+  cursor: pointer;
+
+  // Style options with hidden attribute
+  option[hidden="true"] {
+    color: #f00 !important;
+  }
+`;
+
+export const BackLink = styled(RefreshBlock)`
+  text-decoration: none;
+  color: #000;
 `;
--- src/routes/AddSequence/AddSequence.tsx
+++ src/routes/AddSequence/AddSequence.tsx
@@ -1,3 +1,55 @@
+import { Link } from "react-router-dom";
+import {
+  BackLink,
+  Button,
+  RefreshBlock,
+  Select,
+} from "../../components/styled-components";
+
+import Back from "./../../assets/icons/back-arrow.svg";
+import Refresh from "./../../assets/icons/refresh.svg";
+
 export default function AddSequence() {
-  return <div>AddSequence</div>;
+  return (
+    <div
+      style={{
+        padding: "1em 1em 0 1em",
+      }}
+    >
+      <Link to="/">
+        <BackLink>
+          <img src={Back} alt="back" width={15} height={15} />
+          <span>Back </span>
+        </BackLink>
+      </Link>
+      <h2
+        style={{
+          fontWeight: 500,
+          fontSize: "1.5em",
+          marginBottom: "1.5em",
+        }}
+      >
+        AddSequence
+      </h2>
+      <form>
+        <Select>
+          <option value="" hidden>
+            Choose a sequence
+          </option>
+          <option value="1">Sequence 1</option>
+        </Select>
+        <Select>
+          <option value="" hidden>
+            Recipient
+          </option>
+          <option value="1">Sequence 1</option>
+        </Select>
+        <RefreshBlock>
+          <img src={Refresh} alt="refresh" width={15} height={15} />
+          <span>Refresh Sequesnces</span>
+        </RefreshBlock>
+        <Button>Add Now</Button>
+      </form>
+    </div>
+  );
 }
